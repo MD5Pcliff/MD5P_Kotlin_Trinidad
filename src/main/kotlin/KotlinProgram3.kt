d943bd929f67cba25386ae1dcffaf839918931d3
Clifford <clt.jinyi@gmail.com>
2023-04-11 20:16:18 +0800

    KotlinProgram3

---
+++ src/main/kotlin/KotlinProgram3.kt
@@ -0,0 +1,12 @@
+fun main(){
+    //Input - Process - Output
+    //for String only
+    println("Enter your name:")
+    var name :String = readln()
+    println("Hello $name!!")
+
+    //for Integer only
+    println("Enter a Number:")
+    var number :Int = readln().toInt()
+    println("You have Entered a Number $number!")
+}
