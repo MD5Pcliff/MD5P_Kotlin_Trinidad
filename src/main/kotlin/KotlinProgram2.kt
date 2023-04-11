14d8bfd733305d2f1510507eaf03b2b57fbbd46d
Clifford <clt.jinyi@gmail.com>
2023-04-11 20:16:56 +0800

    KotlinProgram2

--- src/main/kotlin/KotlinProgram2.kt
+++ src/main/kotlin/KotlinProgram2.kt
@@ -1,3 +1,14 @@
 fun main(){
-    print("Welcome to Kodego")
+    var name:String = "John Week"
+    //displaying variable value using string interpolation
+    println("Hello $name")
+    var age:Int = 36
+    var salary:Double = 1000.00
+    var skillLevel:Char = 'S'
+    var isActive:Boolean = true
+    println("His age is $age")
+    println("His salary is $salary")
+    println("His skill level is $skillLevel")
+    println("He is currently acitive? $isActive")
+
 }
