43bdc82a371e65f2710cd24bf6419a2a150b1cd5
Clifford <clt.jinyi@gmail.com>
2023-04-11 20:10:26 +0800

    KotlinActivity1

---
+++ src/main/kotlin/KotlinActivity1.kt
@@ -0,0 +1,94 @@
+fun main(){
+    var pos:String = "Mobile Developer"
+    val dateAvailable:String = "September 2023"
+    var salary:Int = 70000
+    var lastName:String = "Trinidad"
+    var firstName:String = "Clifford"
+    var middleName:String = "Luces"
+    var address:String = "Tondo, Manila"
+    var city:String = "Manila"
+    var state:String = "Philippines"
+    var zipCode:Int = 1013
+    var homeTel:Int = 7088142
+    var cellPhone:Long = 9994210767
+    var email:String = "clt.jinyi@gmail.com"
+    var sss:Int = 777777777
+    var yesOrNo:Boolean = false
+    var education:String = "T.Paez"
+    var schoolAdd:String = "Tondo, Manila"
+    var yearsattended:String = "1999 - 2009"
+    var degree:String = "None"
+    var major:String = "None"
+    var employer:String = "JCD"
+    var dateemployed:String = "2021 - 2023"
+    var duties:String = "Monitoring"
+    println("Application for Employment")
+    println("Position you are applying for: $pos")
+    print("  Desired Salary: $salary")
+    println("Date Available for work: $dateAvailable")
+    println("Personal Information")
+    print("$lastName $firstName $middleName")
+    println("Last Name    First Name     Middle")
+    println("$address $city $state $zipCode")
+    println("Address    City    State   Zip")
+    println("Home Phone: $homeTel Cell Phone: $cellPhone Email address: $email")
+    println("Social Security Number: $sss")
+    println("Are you a U.S Citizen: $yesOrNo")
+    yesOrNo = false
+    println("Have you been convicted of a felony? $yesOrNo")
+    yesOrNo = true
+    println("If selected for employment are you willing to submit to a pre-employment drug screening test? $yesOrNo")
+    println("Education")
+    println("School Name        Location       Years Atternded      Degreee received         Major")
+    println("$education   $schoolAdd      $yearsattended       $degree         $major")
+    education = "MIT"
+    schoolAdd = "Intramuros"
+    yearsattended = "2009 - 2015"
+    degree = "BS CE"
+    major = "Construction"
+    println("$education   $schoolAdd      $yearsattended       $degree         $major")
+    major = "CE Board Passer"
+    println("Other training, Certifications or licenses held:$major")
+    println("Employment")
+    println("Employer: $employer Dates Employed: $dateemployed")
+    homeTel = 28888888
+    salary = 12000
+    println("Work Phone: $homeTel Pay Rate: $salary")
+    address = "Bel Air"
+    city = "Makati"
+    state = "Philippines"
+    zipCode = 1021
+    println("Address: $address        City: $city      State: $state     Zip: $zipCode")
+    pos = "Jr. Civil Engineer"
+    println("Position: $pos")
+    println("Duties Performed: $duties")
+    lastName = "Dela Cruz"
+    firstName = "Juan"
+    pos = "Site Supervisor"
+    println("Supervisors Name and Title: $firstName $lastName $pos")
+    duties = "None"
+    println("Reasons for Leaving: $duties")
+    yesOrNo = true
+    println("May we Contact them: $yesOrNo")
+    firstName = "John"
+    lastName = "Michael"
+    pos = "Engineer"
+    employer = "Megawide"
+    cellPhone = 971555555
+    println("References")
+    println("Name      Title        Company        Phone")
+    println("$firstName $lastName $pos $employer $cellPhone")
+    println("Acknowledgement and Authorization")
+    yesOrNo = true
+    println("I certify that all the answers given herein are true and complete to the best of my knowledge $yesOrNo")
+    yesOrNo = true
+    println("I authorize the investigation of all statements contained in this application for the employment as may be necessary in arriving at an employment decision $yesOrNo")
+    yesOrNo = true
+    println("In the event of employment, I understand that false or misleading infromation given in my application or interview(s) may result in discharge. $yesOrNo")
+    firstName = "Clifford"
+    lastName = "Trinidad"
+    dateemployed = "April 5, 2023"
+    println("$firstName $lastName  $dateemployed")
+    println("Signature of Applicant         Date")
+
+}
