7551659a7e92104960fb4f0d0dd2e46449c7ebb5
Clifford <clt.jinyi@gmail.com>
2023-04-11 20:36:07 +0800

    KotlinActivity1C

---
+++ src/main/kotlin/KotlinActivity1C.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var item:String = readln()
+    var quantity:Int = readln().toInt()
+    var unit:String = readln()
+    var characteristic:String = readln()
+    var categories:String = readln()
+
+}
