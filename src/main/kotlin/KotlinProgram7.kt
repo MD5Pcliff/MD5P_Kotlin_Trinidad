76a04056b687e4580f7260b42f9003ecc58fc492
Clifford <clt.jinyi@gmail.com>
2023-04-14 20:24:42 +0800

    KotlinProgram7

---
+++ src/main/kotlin/KotlinProgram7.kt
@@ -0,0 +1,15 @@
+fun main(){
+    //increment ++ and decerement operators --
+    var x :Int = 1
+    println(x++)
+    //pre increment x++ post increment ++x
+
+    var y :Int = 5
+    //pre decrement --y post decrement y--
+    println(--y)
+
+    //increment specific number +=number of incerement , decrement -=number of decrement
+    var z :Int = 1
+    z-=5
+    println(z)
+}
