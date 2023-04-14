26c1379d0a6786e006bf989dc69131b3f3619dd5
Clifford <clt.jinyi@gmail.com>
2023-04-14 20:27:32 +0800

    PracticeActivity1

---
+++ src/main/kotlin/PracticeActivity1.kt
@@ -0,0 +1,21 @@
+fun main(){
+
+    var base :Int = 0
+    var height :Int = 0
+
+    println("Enter value of base:")
+    base = readln().toInt()
+    println("Enter value of height:")
+    height = readln().toInt()
+    println("Area of Triangle is ${base*height/2}")
+
+    var a :Int = 0
+    var b :Int = 0
+
+    println("Enter value of a:")
+    a = readln().toInt()
+    println("Enter value of b:")
+    b = readln().toInt()
+
+    println("c = ${(2*a+2*(a*b)-4*(a+b))/2}")
+}
