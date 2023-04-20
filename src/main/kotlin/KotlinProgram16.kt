11ea6645152866ba7255701d36ec1cd76b387708
Clifford <clt.jinyi@gmail.com>
2023-04-20 18:42:25 +0800

    KotlinProgram16.kt

---
+++ src/main/kotlin/KotlinProgram16.kt
@@ -0,0 +1,10 @@
+fun main(){
+    //do-while loop
+    //1-loop variable
+    var counter :Int = 0
+    do{
+        println("Cliff")
+        counter++
+    }while(counter<5)//condition
+    println("Hello World")
+}
