1d0995ddcbd199b5e580a21685537619a5b54e60
Clifford <clt.jinyi@gmail.com>
2023-04-20 18:41:41 +0800

    KotlinProgram15

---
+++ src/main/kotlin/KotlinProgram15.kt
@@ -0,0 +1,16 @@
+fun main(){
+    // while loop
+    //1: Loop Variable
+    var counter :Int = 10
+    //2: Condition
+    while(counter > 0){
+        println("$counter")
+        //body
+
+        //update
+        counter--
+
+    }
+    println("Happy New Year")
+
+}
