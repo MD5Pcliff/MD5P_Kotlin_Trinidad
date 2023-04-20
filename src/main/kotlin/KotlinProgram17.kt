a3f8d0274ffde2a4c23c22a481df7f6d039fa141
Clifford <clt.jinyi@gmail.com>
2023-04-20 18:43:07 +0800

    KotlinProgram17

---
+++ src/main/kotlin/KotlinProgram17.kt
@@ -0,0 +1,19 @@
+fun main(){
+    // while loop
+    //1: Loop Variable
+    var counter :Int = 0
+    //2: Condition
+    while(counter < 10){
+        println("Cliff")
+        if(counter == 3){
+            break
+        }
+        //body
+
+        //update
+        counter++
+
+    }
+    println("Hello World")
+
+}
