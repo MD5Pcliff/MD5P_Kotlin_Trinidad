12f13ee551e70c68e62ed16d3c1c90d58281c6c3
Clifford <clt.jinyi@gmail.com>
2023-04-21 18:18:32 +0800

    KotlinProgram20

---
+++ src/main/kotlin/KotlinProgram20.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var ctrl1 = 0
+    while(ctrl1 < 8){
+        var ctrl2 = 0
+        while(ctrl2 < 4){
+            print("*")
+            ctrl2++
+        }
+        println()
+        ctrl1++
+    }
+}
