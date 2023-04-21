3104dc2022a54531ded2d840affccedc5062e636
Clifford <clt.jinyi@gmail.com>
2023-04-21 18:17:42 +0800

    PracticeActivity18

--- src/main/kotlin/PracticeActivity18.kt
+++ src/main/kotlin/PracticeActivity18.kt
@@ -3,14 +3,15 @@ fun main(){
     var n = 0 //Positive Interger
     println("Please Enter a Positive Interger:")
     n =readln().toInt()
-    var x = 1 //additive for odd number
+    var x = 0 //additive for odd number
     var counter = 0 //number of loop
 
     //Process
     repeat(n-(n/2)){
+        x++
         println("${counter+x}") //Computation and Output
         counter++
-        x++
+
 
 
 
