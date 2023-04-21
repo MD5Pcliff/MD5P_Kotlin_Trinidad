ce378766dcdbfef4f221c5aa2632aa00ef34659f
Clifford <clt.jinyi@gmail.com>
2023-04-21 18:20:34 +0800

    PracticeActivity22

---
+++ src/main/kotlin/PracticeActivity22.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var ctrl1 = 0
+    while(ctrl1 < 5){
+        var ctrl2 = 5
+        while(ctrl2 > ctrl1){
+            print("*")
+            ctrl2--
+        }
+        println()
+        ctrl1++
+    }
+}
