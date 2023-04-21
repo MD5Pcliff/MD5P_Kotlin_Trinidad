9476e7a551aaeedec19c333aca3d984f02f8794e
Clifford <clt.jinyi@gmail.com>
2023-04-21 21:12:44 +0800

    PracticeActivity6

---
+++ src/main/kotlin/PracticeActivity6.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var n :Int = 0
+    println("Please Enter a Number:")
+    n = readln().toInt()
+    if(n > 0){
+        println("The number is Positive!")
+    }else if(n < 0){
+        println("The number is Negative!")
+    }else if(n == 0){
+        println("The number is Zero")
+    }
+}
