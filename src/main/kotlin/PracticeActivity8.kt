b4ce85eb70685c85483ddfdf1c172d294b197121
Clifford <clt.jinyi@gmail.com>
2023-04-21 21:44:09 +0800

    PracticeActivity8

---
+++ src/main/kotlin/PracticeActivity8.kt
@@ -0,0 +1,18 @@
+fun main(){
+    var grade :Int = 0
+    println("Please Enter your Exam Score:")
+    grade = readln().toInt()
+
+    if(grade >= 90){
+        println("Your Grade is A")
+    }else if(grade >= 80){
+        println("Your Grade is B")
+    }else if(grade >=70){
+        println("Your Grade is C")
+    }else if(grade >= 60){
+        println("Your Grade is D")
+    }else if(grade < 60){
+        println("Your Grade is F")
+    }
+
+}
