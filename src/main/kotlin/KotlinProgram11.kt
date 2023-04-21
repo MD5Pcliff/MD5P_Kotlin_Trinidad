b235f43b73de14fcb8168fe2c82a45f469baaa0c
Clifford <clt.jinyi@gmail.com>
2023-04-21 18:16:47 +0800

    KotlinProgram11

---
+++ src/main/kotlin/KotlinProgram11.kt
@@ -0,0 +1,24 @@
+fun main(){
+    //if else if statement
+    // age 0 - 12 -> Kids
+    // age 13 - 19 -> Teenagers
+    // age 19 - 59 -> Adult
+    // age 60 and above -> Senior Citizen
+    // less than 0 - invalid age
+    var age :Int = 0
+    println("Please Enter Age:")
+    age = readln().toInt()
+
+    if(age >=60){
+        println("Senior Citizen")
+    }else if(age >=19){
+        println("Adult")
+    }else if(age >=13){
+        println("Teenager")
+    }else if(age >0){
+        println("Kid")
+    }else if(age <=0){
+        println("Invalid Age")
+    }
+
+}
