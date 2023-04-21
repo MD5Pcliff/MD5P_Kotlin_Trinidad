55f3c80dbdbc22b37bfe43b3493912b7913e732c
Clifford <clt.jinyi@gmail.com>
2023-04-21 21:37:56 +0800

    KotlingProgram13

---
+++ src/main/kotlin/KotlinProgram13.kt
@@ -0,0 +1,23 @@
+fun main(){
+    //when statement -> java , c , c# , c++ -> switch statement
+    //digits to word
+    var num :Int = 0
+    println("Enter a Number")
+    num = readln().toInt()
+
+    //when statement does not use conditional operators
+    when(num){
+        1 -> {
+            println("One")
+        }
+        2 -> {
+            println("Two")
+        }
+        3 -> {
+            println("Three")
+        }
+        else -> {
+            println("Invalid Number")
+        }
+    }
+}
