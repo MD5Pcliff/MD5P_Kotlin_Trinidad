33078189b5814da32d95360c6524840e0df8bbd8
Clifford <clt.jinyi@gmail.com>
2023-04-21 23:18:05 +0800

    PracticeActivity13

---
+++ src/main/kotlin/PracticeActivity13.kt
@@ -0,0 +1,21 @@
+fun main(){
+    var weight :Double = 0.0
+    var height :Double = 0.0
+
+    println("Please Enter Weight in Kgs:")
+    weight = readln().toDouble()
+    println("Please Enter Height in Meters:")
+    height = readln().toDouble()
+    var BMI:Double = (weight/(height*height))
+
+    if(BMI >= 30){
+            println("Your BMI is ${String.format("%.2f",BMI)}, which means you are Obese")
+    }else if(BMI > 24.9){
+            println("Your BMI is ${String.format("%.2f",BMI)}, which means you are Overweight")
+    }else if(BMI > 18.5){
+        println("Your BMI is ${String.format("%.2f",BMI)}, which is in the Normal Weight Range")
+    }else if(BMI <= 18.5){
+        println("Your BMI is ${String.format("%.2f",BMI)}, which means your Underweight")
+    }
+
+}
