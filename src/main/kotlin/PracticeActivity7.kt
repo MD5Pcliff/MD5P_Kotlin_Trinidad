73935bb6b27f2a111166963a95ea14fea0f934b1
Clifford <clt.jinyi@gmail.com>
2023-04-21 21:37:18 +0800

    PracticeActivity7

---
+++ src/main/kotlin/PracticeActivity7.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var n :Int = 0
+
+    println("Please Enter a Number:")
+    n = readln().toInt()
+
+
+    if(n %2 == 0){
+        println("The Number is Even")
+    }else{
+        println("The Number is Odd")
+    }
+}
