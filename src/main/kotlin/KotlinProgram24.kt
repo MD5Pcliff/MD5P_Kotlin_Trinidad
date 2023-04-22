ef169bd3f28d1fa2743eab8dc3d9b3b4fab4ef7a
Clifford <clt.jinyi@gmail.com>
2023-04-22 11:03:39 +0800

    KotlinProgram24

---
+++ src/main/kotlin/KotlinProgram24.kt
@@ -0,0 +1,22 @@
+fun main(){
+    //set
+    var numbers = setOf<Int>(1,3,5,7,9)
+    println(numbers.elementAt(0))
+
+    var ctrl :Int = 0
+    while(ctrl < numbers.size) {
+        println(numbers.elementAt(ctrl))
+        ctrl++
+    }
+    println("***************")
+    var numbers2 = mutableSetOf<Int>(1,3,5,7,9)
+    numbers2.add(6)
+    numbers2.remove(3)
+
+    var ctrl2 :Int = 0
+    while(ctrl2 < numbers2.size) {
+        println(numbers2.elementAt(ctrl2))
+        ctrl2++
+    }
+
+}
