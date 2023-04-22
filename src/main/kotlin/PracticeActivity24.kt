af26683f40c1f5327a08197c6bf022056486605b
Clifford <clt.jinyi@gmail.com>
2023-04-22 13:51:14 +0800

    PracticeActivity24

---
+++ src/main/kotlin/PracticeActivity24.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var num = listOf<Int>(2, 4, 6, 8, 10)
+    println("Number   Square   Cube")
+
+    var ctrl :Int = 0
+    var square :Int = 0
+    var cube :Int = 0
+    while(ctrl < num.size) {
+        println("${num[ctrl]}        ${(num[ctrl]) * (num[ctrl])}        ${(num[ctrl]) * (num[ctrl]) * (num[ctrl])}")
+        square = square+((num[ctrl]) * (num[ctrl]))
+        cube = cube + ((num[ctrl]) * (num[ctrl]) * (num[ctrl]))
+        ctrl++
+    }
+    println("Total    $square      $cube")
+
+}
