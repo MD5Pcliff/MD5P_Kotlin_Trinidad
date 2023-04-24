3df77ae8fd4e73cc8e72f6727e5e9d4d7137155b
Clifford <clt.jinyi@gmail.com>
2023-04-24 20:52:40 +0800

    PracticeActivity34

---
+++ src/main/kotlin/PracticeActivity34.kt
@@ -0,0 +1,36 @@
+fun main(){
+    println("Please Enter a String:")
+    var name = readln()
+    var count = 0
+    var counter = 0
+
+    while (count < name.length) {
+        var vowels = name[count].uppercase().contains("a".uppercase())
+        var vowels1 = name[count].uppercase().contains("e".uppercase())
+        var vowels2 = name[count].uppercase().contains("i".uppercase())
+        var vowels3 = name[count].uppercase().contains("o".uppercase())
+        var vowels4 = name[count].uppercase().contains("u".uppercase())
+        if (vowels == true) {
+            counter++
+            count++
+
+        } else if (vowels1 == true) {
+            counter++
+            count++
+        } else if (vowels2 == true) {
+            counter++
+            count++
+        } else if (vowels3 == true) {
+            counter++
+            count++
+        } else if (vowels4 == true) {
+            counter++
+            count++
+        }else{
+            count++
+        }
+        }
+    println(counter)
+    }
+
+
