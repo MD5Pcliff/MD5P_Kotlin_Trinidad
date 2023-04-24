61afb432bd2f34e42146e47b77418d745286e02f
Clifford <clt.jinyi@gmail.com>
2023-04-24 20:50:30 +0800

    KotlinProgram28

---
+++ src/main/kotlin/KotlinProgram28.kt
@@ -0,0 +1,24 @@
+fun main(){
+    //length
+    val mytext = "Hello World!"
+    val stringlength = mytext.length
+    println(stringlength)
+
+    //lowercase and uppercase
+    val uppercase = mytext.uppercase()
+    val lowercase = mytext.lowercase()
+    println(uppercase)
+    println(lowercase)
+
+    //contains
+    val searchtext = "hello"
+    val result = mytext.uppercase().contains(searchtext.uppercase())
+    println(result)
+
+    //replace
+    val replacedtext = mytext.replace("World", "Kotlin")
+    println(replacedtext)
+
+    //accessing index
+    println("${mytext[0]} ${mytext[11]}")
+}
