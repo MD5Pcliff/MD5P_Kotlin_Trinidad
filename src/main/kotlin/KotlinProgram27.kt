7699ac56663cb4328329e84031685c7a75f31ed5
Clifford <clt.jinyi@gmail.com>
2023-04-24 20:51:20 +0800

    KotlinProgram27

---
+++ src/main/kotlin/KotlinProgram27.kt
@@ -0,0 +1,29 @@
+fun main(){
+    // concatenation
+    var firstname = "Juan"
+    var middlename = "D."
+    var lastname = "Dela Cruz"
+    var fullname = firstname + " " + middlename + " " + lastname
+    fullname =firstname.plus(" ").plus(middlename).plus(" ").plus(lastname)
+    println(fullname)
+
+    //interpolation
+    fullname = "$firstname $middlename $lastname"
+    println(fullname)
+
+    //substring
+    var myText = "Hello World!"
+    var subtext = myText.substring(6,12)//var.substring(1st letter index , last letter index)
+    println(subtext)
+
+    //split
+    var text = "Peter, Paul, Mary"
+    var splitText = text.split(",")
+    println(splitText)
+
+
+    //trim
+    var textToTrim = " \n Hello World!"
+    var trimmedText = textToTrim.trim()
+    println(trimmedText)
+}
