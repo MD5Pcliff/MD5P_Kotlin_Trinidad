055352317b0c9921290121fba4e155b97198a8d6
Clifford <clt.jinyi@gmail.com>
2023-04-26 14:32:05 +0800

    KotlinProgram4

---
+++ src/main/kotlin/kotlinprograms/KotlinProgram4.kt
@@ -0,0 +1,18 @@
+package kotlinprograms
+
+fun main (){
+    //arithmetic operations
+    println("This is an example of addtion: ${2+5}")
+    //store in variable
+    var sum :Int = 5 + 8
+    var difference :Int = 5 - 8
+    var product :Int = 5 * 8
+    var qoutient :Int =100/25
+    var modulo :Int = 5 % 2
+
+    println("The sum is $sum")
+    println("The difference is $difference")
+    println("The qoutient is $qoutient")
+    println("The modulo is $modulo")
+
+}
