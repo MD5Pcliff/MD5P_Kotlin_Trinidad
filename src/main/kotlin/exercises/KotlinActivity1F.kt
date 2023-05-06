2492af5bf2cd0e967462f77f8c7679a8079f3d11
Clifford <clt.jinyi@gmail.com>
2023-05-06 21:16:37 +0800

    MagicSquare

---
+++ src/main/kotlin/exercises/KotlinActivity1F.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("Enter string 1:")
+    var string1 = readln()
+    println("Enter string 2:")
+    var string2 = readln()
+    var set1 = mutableSetOf<String>()
+    var set2 = mutableSetOf<String>()
+    for (i in set1){
+
+    }
+}
--- src/main/kotlin/practiceactivities/PracticeActivity36.kt
+++ src/main/kotlin/practiceactivities/PracticeActivity36.kt
@@ -1,52 +1,54 @@
-package practiceactivities
-
-//Not finished
-
 fun main() {
     print("Please Enter Desired Number of Row:")
-    var row = readln().toInt()
+    val row = readln().toInt()
     print("Please Enter Desired Number of Column:")
-    var column = readln().toInt()
+    val column = readln().toInt()
     var count = 0
-    var matrix = mutableListOf<MutableList<Int>>()
-    for (index in 0..row) {
-        repeat(row) {
-            matrix.add(mutableListOf())
-        }
+    val matrix = mutableListOf<MutableList<Int>>()
 
+    repeat(row) {
+        matrix.add(mutableListOf())
+    }
 
-        for (i in count..row - 1) {
-            var col = 0
-            var colList = mutableListOf<Int>()
-            for (j in col..column - 1) {
-                println("Please Enter Number:")
-                colList.add(readln().toInt())
-                col++
-
-            }
-            matrix[count] = colList
-            count++
-
+    for (i in count..row - 1) {
+        var col = 0
+        val colList = mutableListOf<Int>()
+        for (j in col..column - 1) {
+            print("Please Enter Number:")
+            colList.add(readln().toInt())
+            col++
 
         }
-        for (colList in matrix) {
-            for (j in colList)
-                print("$j\t")
-            print("\n")
-
-        }
-        var num = (((row*column)*((row*column)+1)/2))/row
-        println(num)
-        var matrix1 = mutableListOf<Int>()
-        for (index in 0..row){
-            repeat(row){
-                var count3 = 0
-            matrix1.add(matrix.elementAt(count3).sum())
-                count3++
-            }
-        }
-
+        matrix[count] = colList
+        count++
 
 
     }
+    for (colList in matrix) {
+        for (j in colList)
+            print("$j\t")
+        print("\n")
+
+
+    }
+    for (index in matrix) {
+        val i1 = matrix[0][0] + matrix[0][1] + matrix[0][2]
+        val i2 = matrix[1][0] + matrix[1][1] + matrix[1][2]
+        val i3 = matrix[2][0] + matrix[2][1] + matrix[2][2]
+        val j1 = matrix[0][0] + matrix[1][0] + matrix[2][0]
+        val j2 = matrix[0][1] + matrix[1][1] + matrix[2][1]
+        val j3 = matrix[0][2] + matrix[1][2] + matrix[2][2]
+        val d1 = matrix[0][0] + matrix[1][1] + matrix[2][2]
+        val d2 = matrix[2][0] + matrix[1][1] + matrix[0][2]
+        println("$i1   $i2     $i3")
+        println("$j1   $j2     $j3")
+        println("$d1   $d2     ")
+        if (i1 == 15 && i2 == 15 && i3 == 15 && j1 == 15 && j2 == 15 && j3 == 15 && d1 == 15 && d2 == 15) {
+            println("The Matrix is a Magic Square")
+        } else {
+            println("The Matrix is not a Magic Square")
+        }
+    }
 }
+
+
