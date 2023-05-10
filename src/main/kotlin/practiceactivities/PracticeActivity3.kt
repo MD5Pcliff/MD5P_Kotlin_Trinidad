807fddadb503899f3f069fa38af8b2dad3193073
Clifford <clt.jinyi@gmail.com>
2023-05-10 20:53:14 +0800

    PracticeActivities

--- src/main/kotlin/practiceactivities/PracticeActivity3.kt
+++ src/main/kotlin/practiceactivities/PracticeActivity3.kt
@@ -8,7 +8,7 @@ fun main(){
     var average = 0.0
 
     //Process
-    //Avarage = (Sum of All numbers)/(Number of Items)
+    //Average = (Sum of All numbers)/(Number of Items)
     println("Gasoline")
     average = (day1+day2+day3)/3
     println("The average price of gasoline is = ${(String.format("%.2f", average))}")
